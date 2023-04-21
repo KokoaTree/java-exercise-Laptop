55a6b0dad173af4b41c996c04ae3b74242207e43
KokoaTree <4kakao1@daum.net>
2023-04-21 13:29:30 +0900

    feat: MemberVariable - SpaceInvaderMemberVariable

---
+++ src/main/java/week1/day5/SpaceInvaderMemberVariiable.java
@@ -0,0 +1,23 @@
+package week1.day5;
+
+public class SpaceInvaderMemberVariiable {
+    int location; // 멤버변수
+
+    public void moveLeft() {
+        location = location - 1;
+        System.out.printf("moveLeft: %d\n", location);
+    }
+    public void moveRight() {
+        location = location + 1;
+        System.out.printf("moveRight: %d\n", location);
+    }
+
+    public static void main(String[] args) {
+        SpaceInvaderMemberVariiable simv = new SpaceInvaderMemberVariiable();
+        simv.moveLeft();
+        simv.moveRight();
+        simv.moveRight();
+        simv.moveRight();
+        simv.moveRight();
+    }
+}
